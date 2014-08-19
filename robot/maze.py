33c706f87d875242ecc15cc6af516322ca70c1ad
ronanmtkelly <ronank0402@gmail.com>
2014-08-19 14:48:56 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -10,7 +10,10 @@ while touch() != 'fruit':
   if touch() == 'wall':
     if left_side() == 'wall':
       turn(1)
-    else:
-      turn(-1)
+    elif left_side != 'wall':
+      if right_side != 'wall':
+        turn(1)
+      else:
+        turn(-1)
   else:
     move(1)
