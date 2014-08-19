3d5069558776340de64314d4b13629de64163f69
ronanmtkelly <ronank0402@gmail.com>
2014-08-19 12:18:27 +0100

    Closing logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -9,13 +9,16 @@ for i in range(0,4):
   move(400)
   turn(90) 
 
+def gridlines
+  for i in range(0,4):
+    color("black")
+    move(50)
+    turn(90)
+    move(400)
+    turn(270)
+    move(50)
+    turn(270)
+    move(400)
+    turn(90)
+  
 for i in range(0,4):
-  color("black")
-  move(50)
-  turn(90)
-  move(400)
-  turn(270)
-  move(50)
-  turn(270)
-  move(400)
-  turn(90)
