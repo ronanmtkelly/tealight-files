70240429e47818d6664ea4e96fa5b88d55db0cdc
ronanmtkelly <ronank0402@gmail.com>
2014-08-20 09:39:19 +0100

    Running art/sketch.py

--- art/sketch.py
+++ art/sketch.py
@@ -4,13 +4,9 @@ lastx = 0
 lasty = 0
 
 
-def handle_mousedown(x,y):
-  global lastx, lasty
+
+def handle_mousemove(x,y,button):
   
-  lastx = x
-  lasty = y
-
-
   if button == "left":
     color("blue")
     line(lastx, lasty, x, y)
